2ee131958055b658b3b2a0c74880d95ccf3253e5
Facuzanan88 <facundozanandrea@gmail.com>
2022-09-03 10:23:53 -0300

    Checkpoint m2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import CharacterDetail from "./components/CharacterDetail/CharacterDetail";
+import CreateCharacter from "./components/CreateCharacter/CreateCharacter";
+import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
+import Ships from "./components/Ships/Ships";
 
 function App() {
   return (
     <div>
+      <Route path='/'>
+        <Nav />
+      </Route>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route exact path="/character/:id" >
+        <CharacterDetail />
+      </Route>
+      <Route exact path="/character/create" >
+        <CreateCharacter />
+      </Route>
+      <Route exact path="/ships" >
+        <Ships />
+      </Route>
     </div>
   );
 }
